44254dcb79ab865f0521213a706b72b4488a0354
11alexey11 <65157290+11alexey11@users.noreply.github.com>
2020-10-08 21:23:49 +0300

    feat: add decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arraySymbol = [];
+    debugger;
+    for (let i = 0; i < expr.length; i += 10) {
+        let element = MORSE_TABLE[expr
+                                 .slice(i, i+ 10)
+                                 .replace(/00/g, '')
+                                 .replace(/10/g, '.')
+                                 .replace(/11/g, '-')];
+        if (element === undefined) {
+            arraySymbol.push(' ');
+        } else {
+            arraySymbol.push(element);
+        }
+    }
+    return arraySymbol.join('');
 }
 
 module.exports = {
